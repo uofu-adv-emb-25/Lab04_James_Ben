24bec2db1ff2ebb9524ea04115e9fdf2c3736ea3
jdaremes <samesjemerad@gmail.com>
2025-09-24 13:10:07 -0600

    Added pseudocode to signaling.c

--- src/signaling.c
+++ src/signaling.c
@@ -5,10 +5,20 @@
 
 void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
+    /* TASK 2
+    xSemaphoreTake(request);
+    data += 5;
+    xSemaphoreGive(response);
     
+    */
 }
 
-BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response)
+BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
-
+    /*  TASK 1
+    xSemaphoreGive(request);
+    xSemaphoreTake(response);
+    return data;
+    
+    */
 }
