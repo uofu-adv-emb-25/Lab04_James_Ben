962765a969d64195455f2bb2876820199135cf3b
Bean07 <ben.martins537@gmail.com>
2025-09-23 22:20:32 -0600

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
+
+#include "signaling.h"
+
+int main (void)
+{
+
+}
---
+++ src/signaling.c
@@ -0,0 +1,14 @@
+#include "signaling.h"
+
+
+
+
+void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
+{
+    
+}
+
+BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response)
+{
+
+}
