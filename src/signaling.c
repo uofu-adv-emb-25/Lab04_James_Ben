05e1418aa7e51c09b471c1c8ac8c5d6057ac4e38
Bean07 <ben.martins537@gmail.com>
2025-09-29 13:09:13 -0600

    Did some work on semaphore exchanges.

--- src/signaling.c
+++ src/signaling.c
@@ -11,10 +11,10 @@ void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t resp
     
     printf("Task2: From task2, data-> input is %d\n", data->input);
     data->output = data->input + 5;
-    xSemaphoreTake(response, portMAX_DELAY);
-
-    xSemaphoreGive(request);
     xSemaphoreGive(response);
+
+    xSemaphoreTake(response, portMAX_DELAY);
+    xSemaphoreGive(request);
 }
 
 BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
@@ -22,10 +22,11 @@ BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t
     //  TASK 1
     printf("Task1: From task1, data input is: %d\n", data->input);
     xSemaphoreGive(request);
-    xSemaphoreGive(response);
-    vTaskDelay(100);
-    // Can maybe take bfore updating
     xSemaphoreTake(response, portMAX_DELAY);
+
+    xSemaphoreGive(response);
+    xSemaphoreTake(request, portMAX_DELAY);
+    
     return data->output;
     
 }
